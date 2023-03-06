8b02daef2d8a612318ffd924c36a1e42e28ad14c
paxxchal <paschalgabrielacj8@gmail.com>
2023-03-06 09:09:05 +0100

    Fixed some errors

--- monty.h
+++ monty.h
@@ -12,9 +12,9 @@
  */
 typedef struct stack_s
 {
-        int n;
-        struct stack_s *prev;
-        struct stack_s *next;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
 } stack_t;
 
 /**
@@ -27,14 +27,12 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-        char *opcode;
-        void (*f)(stack_t **stack, unsigned int line_number);
+	char *opcode;
+	void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-#include <stdio.h>
 #include <stdlib.h>
-stack_t *push_stack(stack_t **stack, int n);
-void push(stack_t **stack, unsigned int line_number);
-void pall(stack_t **stack, unsigned int line_number);
+#include <stdio.h>
+#include <string.h>
 
 #endif /* MONTY_H */
--- opcode_functions.c
+++ opcode_functions.c
@@ -1,69 +1,53 @@
 #include "monty.h"
-/**
- * push_stack - Pushes an element to the top of the stack.
- * @stack: Double pointer to the head of the stack.
- * @n: Integer to push onto the stack.
- *
- * Return: Pointer to the newly added node.
- */
-stack_t *push_stack(stack_t **stack, int n)
-{
-	stack_t *new_node;
 
-	new_node = malloc(sizeof(stack_t));
-	if (new_node == NULL)
+/**
+ * push - pushes an element to the stack
+ * @stack: pointer to the stack
+ * @line_number: current line number in the file
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+	char *arg = strtok(NULL, " \n\t");
+	int n;
+
+	if (!arg || !isdigit(*arg))
+	{
+		fprintf(stderr, "L%d: usage: push integer\n", line_number);
+		exit(EXIT_FAILURE);
+	}
+
+	n = atoi(arg);
+
+	stack_t *new_node = malloc(sizeof(stack_t));
+	if (!new_node)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
+
 	new_node->n = n;
 	new_node->prev = NULL;
-	if (*stack == NULL)
-	{
-		new_node->next = NULL;
-	}
-	else
-	{
-		new_node->next = *stack;
+	new_node->next = *stack;
+
+	if (*stack)
 		(*stack)->prev = new_node;
-	}
+
 	*stack = new_node;
-
-	return (new_node);
-}
-/**
- * push - Pushes an element to the stack.
- * @stack: Double pointer to the head of the stack.
- * @line_number: Line number in the monty file where the opcode was found.
- */
-void push(stack_t **stack, unsigned int line_number)
-{
-	char *n;
-
-	n = strtok(NULL, " \n\t");
-
-	if (n == NULL || !is_integer(n))
-	{
-		fprintf(stderr, "L%u: usage: push integer\n", line_number);
-		exit(EXIT_FAILURE);
-	}
-	push_stack(stack, atoi(n));
 }
 
 /**
- * pall - Prints all the values on the stack.
- * @stack: Pointer to the head of the stack.
- * @line_number: Line number in the monty file where the opcode was found.
+ * pall - prints all the values on the stack
+ * @stack: pointer to the stack
+ * @line_number: current line number in the file
  */
 void pall(stack_t **stack, unsigned int line_number)
 {
-	stack_t *current = *stack;
-
 	(void)line_number;
+	stack_t *temp = *stack;
 
-	while (current != NULL)
+	while (temp)
 	{
-		printf("%d\n", current->n);
-		current = current->next;
+		printf("%d\n", temp->n);
+		temp = temp->next;
 	}
 }
