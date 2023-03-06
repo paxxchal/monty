32bc73ea7434dba4fd69a0d631ed4ef1e2914076
paxxchal <paschalgabrielacj8@gmail.com>
2023-03-06 10:01:37 +0100

    Separated files

---
+++ push.c
@@ -0,0 +1,36 @@
+#include "monty.h"
+
+/**
+ * push - pushes an element to the stack
+ * @stack: double pointer to the beginning of the stack
+ * @line_number: line number being executed from script file
+ *
+ * Return: void
+ */
+void push(stack_t **stack, unsigned int line_number)
+{
+        char *arg = strtok(NULL, " \n\t");
+        stack_t *new_node;
+
+        if (!arg || !isdigit(*arg))
+        {
+                fprintf(stderr, "L%d: usage: push integer\n", line_number);
+                exit(EXIT_FAILURE);
+        }
+
+        new_node = malloc(sizeof(stack_t));
+        if (!new_node)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                exit(EXIT_FAILURE);
+        }
+
+        new_node->n = atoi(arg);
+        new_node->next = *stack;
+        new_node->prev = NULL;
+
+        if (*stack)
+                (*stack)->prev = new_node;
+
+        *stack = new_node;
+}
