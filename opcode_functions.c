039acf277600d7a82d2e601ed072b7768dfaddad
paxxchal <paschalgabrielacj8@gmail.com>
2023-03-06 06:24:37 +0100

    Added some files

---
+++ opcode_functions.c
@@ -0,0 +1,61 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "monty.h"
+
+/**
+ * push - Pushes an element to the stack.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number of the push operation.
+ * @n: The value to push onto the stack.
+ *
+ * Return: N/A.
+ */
+void push(stack_t **stack, unsigned int line_number, int n)
+{
+        stack_t *new_node;
+
+        new_node = malloc(sizeof(stack_t));
+        if (new_node == NULL)
+        {
+                fprintf(stderr, "Error: malloc failed\n");
+                exit(EXIT_FAILURE);
+        }
+
+        new_node->n = n;
+        new_node->prev = NULL;
+
+        if (*stack == NULL)
+        {
+                new_node->next = NULL;
+        }
+        else
+        {
+                new_node->next = *stack;
+                (*stack)->prev = new_node;
+        }
+
+        *stack = new_node;
+
+        (void)line_number; /*Silence the unused parameter warning*/
+}
+
+/**
+ * pall - Prints all the values on the stack, starting from the top.
+ * @stack: A pointer to the top of the stack.
+ * @line_number: The line number of the pall operation.
+ *
+ * Return: N/A.
+ */
+void pall(stack_t **stack, unsigned int line_number)
+{
+        stack_t *current = *stack;
+
+        (void)line_number;
+
+        while (current != NULL)
+        {
+                printf("%d\n", current->n);
+                current = current->next;
+        }
+}
