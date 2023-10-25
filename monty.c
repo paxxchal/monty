593e48fcb47575d7c39969f12ad1eee10e0b7d23
paxxchal <paschalgabrielacj8@gmail.com>
2023-10-25 10:52:27 +0100

    Fixed betty errors

--- monty.c
+++ monty.c
@@ -1,47 +1,24 @@
 #include "monty.h"
 
 /**
- * main - Entry point for the Monty interpreter.
- * @argc: Number of command-line arguments.
- * @argv: Array of command-line arguments.
- *
- * This function reads and interprets Monty byte code files.
- *
- * Return: 0 on success, EXIT_FAILURE on error.
+ * processMontyFile - Read and interpret a Monty bytecode file line by line.
+ * @file: A pointer to the Monty bytecode file.
+ * @stack: A pointer to the stack.
  */
-int main(int argc, char *argv[])
+void processMontyFile(FILE *file, stack_t **stack)
 {
-	FILE *file;
-	stack_t *stack = NULL;
 	char *line = NULL;
-	size_t len = 256; /* You can change this value as needed */
+	size_t len = 256;
 	unsigned int line_number = 0;
 	char *token;
 
-	/* Check the number of command-line arguments */
-	if (argc != 2)
-	{
-		fprintf(stderr, "USAGE: monty file\n");
-		exit(EXIT_FAILURE);
-	}
-
-	/* Open the Monty byte code file */
-	file = fopen(argv[1], "r");
-	if (file == NULL)
-	{
-		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-		exit(EXIT_FAILURE);
-	}
-
-	/* Allocate memory for the line variable */
-	line = malloc(len);
+	line = (char *)malloc(len);
 	if (line == NULL)
 	{
 		fprintf(stderr, "Error: malloc failed\n");
 		exit(EXIT_FAILURE);
 	}
 
-	/* Read and interpret the Monty byte code file line by line */
 	while (fgets(line, len, file) != NULL)
 	{
 		line_number++;
@@ -54,7 +31,7 @@ int main(int argc, char *argv[])
 				/* Execute the push opcode */
 				token = strtok(NULL, " \n\t");
 				if (token != NULL)
-					push(&stack, atoi(token), line_number);
+					push(stack, atoi(token), line_number);
 				else
 				{
 					fprintf(stderr, "L%u: usage: push integer\n", line_number);
@@ -64,23 +41,53 @@ int main(int argc, char *argv[])
 			else if (strcmp(token, "pall") == 0)
 			{
 				/* Execute the pall opcode */
-				pall(&stack, line_number);
+				pall(stack, line_number);
 			}
-			/*Add more opcode cases as needed for other instructions*/
 		}
 	}
-
-	/* Free resources and close the file */
 	free(line);
+}
+
+/**
+ * main - Entry point for the Monty interpreter.
+ * @argc: Number of command-line arguments.
+ * @argv: Array of command-line arguments.
+ *
+ * This function reads and interprets Monty byte code files.
+ *
+ * Return: 0 on success, EXIT_FAILURE on error.
+ */
+int main(int argc, char *argv[])
+{
+	FILE *file;
+	stack_t *stack = NULL;
+
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(EXIT_FAILURE);
+	}
+
+	file = fopen(argv[1], "r");
+	if (file == NULL)
+	{
+		fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+		exit(EXIT_FAILURE);
+	}
+
+	processMontyFile(file, &stack);
+
+	/* Close the file */
 	fclose(file);
 
 	/* Free the stack if not empty */
 	while (stack != NULL)
 	{
 		stack_t *tmp = stack;
+
 		stack = stack->next;
 		free(tmp);
 	}
 
-	return 0;
+	return (0);
 }
--- monty.h
+++ monty.h
@@ -32,6 +32,7 @@ typedef struct instruction_s
 /* Function prototypes */
 void push(stack_t **stack, int value, unsigned int line_number);
 void pall(stack_t **stack, unsigned int line_number);
+void processMontyFile(FILE *file, stack_t **stack);
 /* Add more function prototypes for other opcodes */
 
 #endif /* MONTY_H */
